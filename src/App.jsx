ce608857d47ccf6a2159381050364c4f728a8f75
shraddha ravankar <ravankarshraddha11@gmail.com>
2025-07-18 16:55:24 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from './chai';
+
+function App() {
+  
+  return (
+    <>
+    <Chai/>
+    <h1 className='bg-green-400'>Tailwind Test</h1>
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h3>chai is reday</h3>
+    )
+}
+
+export default Chai;
